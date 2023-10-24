860139732a5b18d2185a6a980c3c0d5c55e9b68a
baehaeyong <baehaeyong@gmail.com>
2023-10-24 17:34:01 +0900

    최신커밋임

--- nsh4352.py
+++ nsh4352.py
@@ -2,14 +2,14 @@ for i in range(10):
     print(i)
 
 #while문
-i=0
-whilei<10:
+i = 0
+while i < 10:
 	print(i)
-	i+=1
+	i += 1
 
 #while문2
 while True:
-	if i ==10:
+	if i == 10:
 		break
 	print(i)
-	i+=1
+	i += 1
