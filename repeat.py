493af2473ab48f26083071e1462a8f974561c937
baehaeyong <baehaeyong@gmail.com>
2023-10-24 16:31:14 +0900

    프로젝트

---
+++ repeat.py
@@ -0,0 +1,17 @@
+#반복문(for, while)
+for i in range(10):
+    print(i)
+
+#while 문
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+#while문(2)
+i = 0
+while True:
+    if i == 10:
+        break
+    print(i)
+    i += 1
